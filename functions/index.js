0d84eb59fb26987a608ee0a272abce0395033842
granttscott <78437619+granttscott@users.noreply.github.com>
2024-10-01 08:01:47 -0700

    com

--- functions/index.js
+++ functions/index.js
@@ -1,63 +1,19 @@
-import express from "express";
-import axios from "axios";
-import { https } from 'firebase-functions';
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
 
-// import path from "path";
-// import { fileURLToPath } from 'url';
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
 
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
 
-
-const app = express();
-const port = 3000;
-const API_URL = "https://api.core.ac.uk";
-
-const APIKey = "YgQwynm1XsNz9KAok4rB2OJSHvLhq5Up";
-// Get the directory name of the current module
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
-app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-    res.render("index.ejs");
-  });
-
-  app.get("/papers", async (req, res) => {
-    res.render("papers.ejs", { papers: [], content: "Please find a paper." });
-  });
-
-  app.get("/search", async (req, res) => {
-    const entityType = req.query.category
-    try {
-      const result = await axios.get(`${API_URL}/v3/search/${entityType}/`);
-      const papers = result.data.results.map(item => ({
-        title: item.title,
-        subjects: item.subjects,
-        url: item.identifiers.find(id => id.startsWith('url:')).split('url:')[1],
-        issn: item.identifiers.find(id => id.startsWith('issn:')).split('issn:')[1]
-      }));      res.render("papers.ejs", { papers });
-    } catch (error) {
-      res.status(404).send(error.message);
-    }
-  });
-
-  app.get("/more", async (req, res) => {
-    const identifier = req.query.issn
-    try {
-      const papers = await axios.get(`${API_URL}/v3/journals/issn:${identifier}`);
-      // const papers = result.data.results.map(item => ({
-      //   title: item.title,
-      //   subjects: item.subjects,
-      //   url: item.identifiers.find(id => id.startsWith('url:')).split('url:')[1]
-      // }));
-            res.render("papers.ejs", { papers });
-    } catch (error) {
-      res.status(404).send(error.message);
-    }
-  });
-
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
-
-  export const api = https.onRequest(app);
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
---
+++ hold/index.js
@@ -0,0 +1,63 @@
+import express from "express";
+import axios from "axios";
+import { https } from 'firebase-functions';
+
+// import path from "path";
+// import { fileURLToPath } from 'url';
+
+
+
+const app = express();
+const port = 3000;
+const API_URL = "https://api.core.ac.uk";
+
+const APIKey = "YgQwynm1XsNz9KAok4rB2OJSHvLhq5Up";
+// Get the directory name of the current module
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+  });
+
+  app.get("/papers", async (req, res) => {
+    res.render("papers.ejs", { papers: [], content: "Please find a paper." });
+  });
+
+  app.get("/search", async (req, res) => {
+    const entityType = req.query.category
+    try {
+      const result = await axios.get(`${API_URL}/v3/search/${entityType}/`);
+      const papers = result.data.results.map(item => ({
+        title: item.title,
+        subjects: item.subjects,
+        url: item.identifiers.find(id => id.startsWith('url:')).split('url:')[1],
+        issn: item.identifiers.find(id => id.startsWith('issn:')).split('issn:')[1]
+      }));      res.render("papers.ejs", { papers });
+    } catch (error) {
+      res.status(404).send(error.message);
+    }
+  });
+
+  app.get("/more", async (req, res) => {
+    const identifier = req.query.issn
+    try {
+      const papers = await axios.get(`${API_URL}/v3/journals/issn:${identifier}`);
+      // const papers = result.data.results.map(item => ({
+      //   title: item.title,
+      //   subjects: item.subjects,
+      //   url: item.identifiers.find(id => id.startsWith('url:')).split('url:')[1]
+      // }));
+            res.render("papers.ejs", { papers });
+    } catch (error) {
+      res.status(404).send(error.message);
+    }
+  });
+
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+
+  export const api = https.onRequest(app);
